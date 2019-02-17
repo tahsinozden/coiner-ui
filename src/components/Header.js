49080ad2f7c3f8113418f52f4079d44565ad9bda
tahsinozden <tahsinozden@gmail.com>
2019-02-17 17:37:57 +0100

    fixed header load issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from "react-bootstrap/es/Navbar";
 import Nav from "react-bootstrap/es/Nav";
 
-const Header = (
-    <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">{'Coiner'}</Navbar.Brand>
-        <Nav className="mr-auto">
-            <Nav.Link href="logout">Logout</Nav.Link>
-        </Nav>
-    </Navbar>
-);
+const Header = () => {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">{'Coiner'}</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="logout">Logout</Nav.Link>
+            </Nav>
+        </Navbar>
+    );
+};
 
 export default Header;
